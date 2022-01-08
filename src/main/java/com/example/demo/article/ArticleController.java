5e38f155cf68194a8866ab8a9cd6297505c9f356
KumKeeHyun <kbzjung359@gmail.com>
2022-01-08 14:37:59 +0900

    컨트롤러 구현

---
+++ src/main/java/com/example/demo/article/ArticleController.java
@@ -0,0 +1,35 @@
+package com.example.demo.article;
+
+import com.example.demo.user.Picker;
+import com.example.demo.user.PickerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/article")
+public class ArticleController {
+
+    private final ArticleService articleService;
+    private final PickerRepository pickerRepository;
+
+    @GetMapping("")
+    @Transactional
+    public List<ArticleDto.Response> getAllArticles() {
+        return ArticleDto.Response.ofList(articleService.getAllArticles());
+    }
+
+    @GetMapping("/{articleId}/pick/{pickId}")
+    @Transactional
+    public String pick(@PathVariable Long articleId, @PathVariable Long pickId) throws Exception {
+        Picker kim = pickerRepository.findByName("Kim").get();
+        articleService.pick(articleId, pickId, kim);
+        return "success";
+    }
+}
