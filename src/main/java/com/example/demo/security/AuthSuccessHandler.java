b5a31e1451f0f3c009a3a25bffd908e3e3c7bc50
KumKeeHyun <kbzjung359@gmail.com>
2022-01-24 19:26:23 +0900

    add example article

--- src/main/java/com/example/demo/security/AuthSuccessHandler.java
+++ src/main/java/com/example/demo/security/AuthSuccessHandler.java
@@ -1,5 +1,7 @@
 package com.example.demo.security;
 
+import com.example.demo.domain.user.entity.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -14,8 +16,11 @@ public class AuthSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
+        User principal = (User) authentication.getPrincipal();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        response.getWriter().println(objectMapper.writeValueAsString(principal));
         response.setContentType("application/json;charset=UTF-8");
         response.setStatus(HttpStatus.OK.value());
-        response.getWriter().println("LoginSuccessful");
     }
 }
